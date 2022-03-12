e1df7965ae6037c0f391a540ccc6f9a9889c9e20
Ishan Kesarwani <ishankesarwani1999@gmail.com>
2022-03-12 11:53:54 +0530

    Complete Project PMS
    Added PMS project.

---
+++ POD-2/Daily-Share-Price/Daily-Share-Price/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into share_details values ('AMZ','Amazon',10500.0);
+insert into share_details values ('GGL','Google',20000.0);
+insert into share_details values ('ABC','Alpabet',13000.0);
+insert into share_details values ('FFB','FaceBook',14000.0);
